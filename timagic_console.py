4c7e889209628714a63a78baa063e54a62351437
Hendrik Elsner <hendrik.elsner@hpm-kommunikation.de>
2014-03-11 16:32:58 +0100

    can now install ipa to multiple connected ios devices

--- timagic_console.py
+++ timagic_console.py
@@ -50,29 +50,37 @@ def get_from_xml(xml_path, key):
 	return value
 
 def remove_apk(**kwargs):
-	print '...trying to uninstall old APK from ' + kwargs['device_id']
+	print '...trying to uninstall APK from ' + kwargs['device_id']
 	shell_exec([settings['adb_path'], '-s', kwargs['device_id'], 'uninstall', kwargs['app_id']])
 
-def install_apk(device_id, apk_path):
-	print '...trying to install new APK to ' + device_id
-	shell_exec([settings['adb_path'], '-s', device_id, 'install', apk_path])
+def install_apk(**kwargs):
+	print '...trying to install APK to ' + kwargs['device_id']
+	shell_exec([settings['adb_path'], '-s', kwargs['device_id'], 'install', kwargs['apk_path']])
 
-def start_app_activity(device_id, app_id, app_name):
-	print '...trying to launch Activity on ' + device_id
-	shell_exec([settings['adb_path'], '-s', device_id, 'shell', 'am', 'start', '-n', (app_id + '/' + app_id + '.' + app_name + 'Activity')])
+def start_app_activity(**kwargs):
+	print '...trying to launch Activity on ' + kwargs['device_id']
+	shell_exec([settings['adb_path'], '-s', kwargs['device_id'], 'shell', 'am', 'start', '-n', (kwargs['app_id'] + '/' + kwargs['app_id'] + '.' + kwargs['app_name_no_spaces'] + 'Activity')])
 
 def deploy_apk(**kwargs):
 	''' installs an apk to all connected adb devices and removes the old version if necessary '''
 	remove_apk(device_id=kwargs['device_id'],app_id=kwargs['app_id'])
-	install_apk(kwargs['device_id'], kwargs['apk_path'])
-	start_app_activity(kwargs['device_id'], kwargs['app_id'], kwargs['app_name_no_spaces'])
+	install_apk(device_id=kwargs['device_id'], apk_path=kwargs['apk_path'])
+	start_app_activity(device_id=kwargs['device_id'], app_id=kwargs['app_id'], app_name_no_spaces=kwargs['app_name_no_spaces'])
+
+def install_ipa(**kwargs):
+	# @TODO
+	print '...trying to install IPA to ' + kwargs['device_id']
+	shell_exec(['ideviceinstaller', '-U', kwargs['device_id'], '-i', kwargs['ipa_path']])
+
+def remove_ipa(**kwargs):
+	# @TODO
+	print '...trying to uninstall IPA from ' + kwargs['device_id']
+	shell_exec(['ideviceinstaller', '-U', kwargs['device_id'], '-u', kwargs['app_id']])
 
 def print_ui():
 	''' prints out the ui '''
 	shell_exec('clear')
-	print ''
 	print color('aqua', '-'*20 + '= TiMagic '+__version__+' =' + '-'*20)
-	print color('aqua', ' '*2 + '! Please setup your paths in timagic_settings.xml !\n')
 	for i in range(len(projects)):
 		print color('green', str(i)) + ' < ' + projects[i]
 	print color('yellow', 'h < show help')
@@ -84,25 +92,32 @@ def print_help():
 	shell_exec('clear')
 	print ''
 	print color('yellow', '-'*20 + '= TiMagic HELP ' + '=' + '-'*20)
+	if not has_libimobiledevice:
+		print '! To unlock additional features install https://github.com/benvium/libimobiledevice-macosx !'
 	print ''
 	print color('yellow', 'project number is represented by *')
 	print ''
+	print ' Common ' + '-'*60
+	if has_libimobiledevice:
+		print color('green','*') + ' | ' + color('green','*build') + '[IOS_VERSION]' + ('\t'*2) + ': installs to all connected devices'
+	else:
+		print color('green','*') + ('\t'*4) + ': installs to all connected android devices'
+	print '*' + color('green','remove') + ('\t'*4) + ': removes the app from all connected ' + ('' if has_libimobiledevice else 'android ') + 'devices'
+	print '*' + color('green','clean') + ('\t'*4) + ": cleans the project's build directory"
+	print ''
 	print ' Android ' + '-'*60
-	print color('green','*') + ('\t'*4) + ': installs to all connected android devices'
-	print '*' + color('green','todevice') + ('\t'*3) + ': installs unsigned APK from previous build to all connected android devices'
 	print '*' + color('green','apk') + ('\t'*4) +': creates a signed APK'
-	print '*' + color('green','todeviceapk') + ('\t'*3) + ': installs signed APK from previous build to all connected android devices'
-	print '*' + color('green','remove') + ('\t'*4) + ': removes the app from all connected android devices'
+	print '*' + color('green','todevice') + ('\t'*3) + ': installs signed APK from previous build to all connected android devices'
+	print '*' + color('green','todeviceU') + ('\t'*3) + ': installs unsigned APK from previous build to all connected android devices'
 	print ''
 	print ' iOS ' + '-'*60
-	print '*' + color('green','iphone') + '|' + color('green','ipad') + '[IOS_VERSION]' + ('\t'*1) + ': launches the iphone or ipad simulator for the given project'
+	print '*' + color('green','iphone') + ' | ' + color('green','ipad') + '[IOS_VERSION]' + ('\t'*1) + ': launches the iphone or ipad simulator for the given project'
 	print '*' + color('green','ipa') + '[IOS_VERSION]' + ('\t'*2) + ': creates an ad-hoc IPA'
-	print '*' + color('green','todeviceipa')+ ('\t'*3) + ': installs ad-hoc IPA from previous build to a connected iOS device [requires libimobiledevice]'
-	print ''
-	print ' Common ' + '-'*60
-	print '*' + color('green','clean') + ('\t'*4) + ": cleans the project's build directory"
+	if has_libimobiledevice:
+		print '*' + color('green','todevice') + ('\t'*3) + ': installs ad-hoc IPA from previous build to all connected iOS devices'
 	print '\n\n'
 
+# get settings from settings file
 config_xml_path = 'timagic_settings.xml'
 settings = {
 	'user_dir': '',
@@ -120,18 +135,27 @@ settings = {
 for elem in settings:
 	settings[elem] = get_from_xml(config_xml_path, elem)
 
-# UI
+# UI variables
 answer = -1
-notification = '\n'*2
+notification = '\n'
 projects = get_project_names()
 
-# ui-loop
+# check if libimobiledevice is installed
+has_libimobiledevice = False
+try:
+	subprocess.call('idevice_id', stdout=subprocess.PIPE)
+	has_libimobiledevice = True
+except:
+	print 'libimobiledevice is not installed...'
+
+# UI-loop
 while True:
 	# print the ui
 	print_ui()
 	user_input = raw_input(last_choice + ' > ')
 	project_num = ''
 	ios_version = ''
+	parallel_processes = {}
 
 	# print help
 	if str(user_input) == 'h':
@@ -139,7 +163,7 @@ while True:
 		raw_input('press enter to close help...')
 		continue
 
-	# last repeat last choice?
+	# repeat last choice?
 	if str(user_input) == '' and str(last_choice) != '':
 		user_input = last_choice
 	# set last_choice from current and strip clean
@@ -176,45 +200,83 @@ while True:
 		ios_version = ios_version or '7.0' # default to iOS-version 7.0
 		app_path = settings['titanium_workspace_path'] + project_name
 
-		# get list of connected android devices
-		device_list = subprocess.check_output(settings['adb_path'] + ' devices | grep device', shell=True).replace('\tdevice','').split('\n')[1:-1]
+		# get lists of connected devices
+		android_device_list = subprocess.check_output(settings['adb_path'] + ' devices | grep device', shell=True).replace('\tdevice','').split('\n')[1:-1]
+		ios_device_list = subprocess.check_output('idevice_id -l', shell=True).split('\n')[0:-1] if has_libimobiledevice else []
 
 		# base titanium cli command
 		base_command = ['titanium', 'build', '-d', app_path, '-s', sdk_version]
 
-		if input_params in ['', 'todevice', 'todeviceapk', 'remove'] :
-			if len(device_list):
-				if input_params == '':
+		if input_params in ['', 'build', 'todevice', 'todeviceU', 'remove']:
+			#
+			# handle android platform
+			#
+			if len(android_device_list):
+				if input_params in ['', 'build']:
 					# default build for android devices
-					print 'building APK'
+					print 'building unsigned APK'
 					shell_exec(base_command + ['-p', 'android', '-b'])
 
-				if input_params == 'todeviceapk':
+				if input_params == 'todevice':
 					apk_path = settings['apk_output_path'] + app_name_escaped_spaces + '.apk'
 				else:
 					apk_path = app_path +'/build/android/bin/' + (app_name_no_spaces if float(sdk_version[0:3]) >= 3.2 else 'app') + '.apk'
 
-				kwargs = {
-					'app_id': app_id
-				}
+
+				kwargs = { 'app_id': app_id }
+				operation = ''
 
 				if input_params == 'remove':
+					operation = 'removing from'
 					target = remove_apk
 				else:
+					operation = 'installing to'
 					target = deploy_apk
 					kwargs['apk_path'] = apk_path
 					kwargs['app_name_no_spaces'] = app_name_no_spaces
 
-				# install the apk to all connected devices
-				print 'installing ' + project_name + ' APK to all devices...'
-				d = {}
-				for device_id in device_list:
+				# install the apk to all connected android devices
+				print project_name + ' ' + operation + ' all android devices...'
+				for device_id in android_device_list:
 					kwargs['device_id'] = device_id
-					d[device_id] = Process(target=target, kwargs=kwargs)
-					d[device_id].start()
-				for device_id in device_list:
-					d[device_id].join()
-			else:
+					parallel_processes[device_id] = Process(target=target, kwargs=kwargs)
+					parallel_processes[device_id].start()
+				for device_id in android_device_list:
+					parallel_processes[device_id].join()
+			#
+			# handle ios platform
+			#
+			if len(ios_device_list):
+				if input_params in ['', 'build']:
+					# default build for ios devices
+					print 'building ad-hoc IPA'
+					shell_exec(base_command + ['-p', 'ios', '-R', settings['distribution_name'], '-I', ios_version, '-P', settings['pp_uuid'], '-O', settings['ipa_output_path'], '-T', 'dist-adhoc'])
+
+				ipa_path = settings['ipa_output_path'] + app_name_escaped_spaces + '.ipa'
+
+				print ipa_path
+				kwargs = {}
+				operation = ''
+
+				if input_params == 'remove':
+					operation = 'removing from'
+					target = remove_ipa
+					kwargs['app_id'] = app_id
+				else:
+					operation = 'installing to'
+					target = install_ipa
+					kwargs['ipa_path'] = ipa_path
+
+				# install the ipa to all connected ios devices
+				print project_name + ' ' + operation + ' all iOS devices...'
+				for device_id in ios_device_list:
+					kwargs['device_id'] = device_id
+					parallel_processes[device_id] = Process(target=target, kwargs=kwargs)
+					parallel_processes[device_id].start()
+				for device_id in ios_device_list:
+					parallel_processes[device_id].join()
+
+			if not (len(android_device_list) or len(ios_device_list)):
 				notification =  color('red', '\n --> Please connect a device first.\n')
 
 		elif input_params == 'clean':
@@ -223,10 +285,6 @@ while True:
 		elif input_params == 'ipa':
 			# build ad-hoc IPA for given iOS-version (e.g. for project number 1 and iOS-version 7.0 --> 1ipa7.0)
 			shell_exec(base_command + ['-p', 'ios', '-R', settings['distribution_name'], '-I', ios_version, '-P', settings['pp_uuid'], '-O', settings['ipa_output_path'], '-T', 'dist-adhoc'])
-
-		elif input_params == 'todeviceipa':
-			shell_exec(['ideviceinstaller', '-i', settings['ipa_output_path'] + app_name_escaped_spaces + '.ipa'])
-			# shell_exec('idevicesyslog') # run syslog after install
 
 		elif input_params == 'ipad' or input_params == 'iphone':
 			# Install to and launch given iOS-Simulator (e.g. for project number 1 and iOS-version 7.0 --> 1ipad7.0 or 1iphone7.0)
